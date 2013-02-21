47987dc404cc9f2d22a75be50c9e70ca1269cfc2
maijou2501 <maijou2501@yahoo.co.jp>
2013-02-21 23:13:27 +0900

    3rd modify splash.sh

--- splash.sh
+++ splash.sh
@@ -3,6 +3,5 @@
 sudo apt-get install plymouth-theme-kubuntu-logo
 sudo update-alternatives --config default.plymouth
 mv ./usasan*png /lib/plymouth/themes/kubuntu-logo/
-mv ./splash.sh  /lib/plymouth/themes/kubuntu-logo/
 mv ./kubuntu-logo.script /lib/plymouth/themes/kubuntu-logo/
 sudo update-initramfs -u
